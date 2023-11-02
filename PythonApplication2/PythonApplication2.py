45d7d1beb71d779319ea772f7aaec21fd65fa29d
user <user@user.com>
2023-11-02 15:02:00 +0300

    222

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,8 @@
+
+print('What is your name?')
+name = input('')
+print('How old are you?')
+age = input('')
+print('Where are you live?')
+sity = input('')
+print('This is '+ name + ' It is '+ age + ' he live in '+ sity )
